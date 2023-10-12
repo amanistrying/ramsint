c7e9e77e7213945f12e4af3d0194ff043430e550
Aman Gupta <aupta2002@gmail.com>
2023-10-12 12:04:03 +0530

    slider done

---
+++ trial.js
@@ -0,0 +1,4 @@
+var clara = document.getElementsByClassName("carousel-inner");
+var ele = document.getElementsByClassName("card").offsetWidth;
+var nbut = document.getElementsByClassName("carousel-control-next-icon");
+var sscrol = 0;
